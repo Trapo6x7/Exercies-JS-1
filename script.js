bd2c86fbe70d7c061ad00d1cf33a8c5638b011bb
Trapo6x7 <matoufdanslezoo@gmail.com>
2024-11-13 16:47:56 +0100

    Exo9

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 let coucou = alert("COUCOU");
 
-console.log(" ")
+console.log(" ");
 
 let lastname = "Doe";
 let firstname = "John";
 let city = "New York";
 
-window.confirm("Nom: "+lastname+"\n"+"Prénom: "+firstname+"\n"+"Ville: "+city);
+window.confirm(
+  "Nom: " + lastname + "\n" + "Prénom: " + firstname + "\n" + "Ville: " + city
+);
 
 console.log(" ");
 
 let prenom = prompt("Comment t'appelles tu?");
-let bonjour = alert("Bonjour "+prenom);
+let bonjour = alert("Bonjour " + prenom);
 
 console.log(" ");
 
@@ -19,7 +21,16 @@ let lastnamereturn = prompt("Ton nom");
 let firstnamereturn = prompt("Ton prénom");
 let cityreturn = prompt("Ta ville");
 
-window.confirm("Nom: "+lastnamereturn+"\n"+"Prénom: "+firstnamereturn+"\n"+"Ville: "+cityreturn);
+window.confirm(
+  "Nom: " +
+    lastnamereturn +
+    "\n" +
+    "Prénom: " +
+    firstnamereturn +
+    "\n" +
+    "Ville: " +
+    cityreturn
+);
 
 console.log(" ");
 
@@ -27,11 +38,63 @@ let decimal1 = prompt("Donnez moi un nombre a virgule");
 let decimal2 = prompt("Donnez moi un autre nombre a virgule");
 
 let num1 = parseInt(decimal1);
-let num2 = parseInt(decimal2);
 
 let entier1 = Math.round(num1);
-let entier2 = Math.round(num2);
 
-window.alert(entier1 * entier2);
+
+window.alert(entier1 * decimal2);
 
 console.log(" ");
+
+let numero1 = prompt("Donnez moi un nombre");
+let numero2 = prompt("Donnez moi un autre nombre");
+
+let numchange1 = parseInt(numero1);
+let numchange2 = parseInt(numero2);
+
+let entiergui1 = Math.trunc(numchange1);
+let entiergui2 = Math.trunc(numchange2);
+
+window.alert(entiergui1 % entiergui2);
+
+console.log(" ");
+
+let prompt1 = prompt("Pointure de chaussures");
+let prompt2 = prompt("Année de naissance");
+
+let numchanger1 = parseInt(prompt1);
+let numchanger2 = parseInt(prompt2);
+
+function calcul(a, b) {
+  return (a * 2 + 5) * 50 - b + 1766;
+}
+
+window.alert(calcul(numchanger1, numchanger2));
+
+console.log(" ");
+
+let adulte = prompt("Quel age avez vous?");
+
+let numchangeradulte = parseInt(adulte);
+
+if (numchangeradulte >= 18) {
+  window.alert("vous etes majeur");
+} else {
+  window.alert("vous etes mineur");
+}
+
+console.log(" ");
+
+let multiplieur = prompt("Donnez moi un nombre?");
+
+let changerprompt = parseInt(multiplieur);
+
+function multiplier(a) {
+  for (let i = 1; i < 10; i = i + 1) {
+    return a * i;
+  };
+};
+
+let resultat = multiplier(changerprompt);
+
+window.alert(resultat);
